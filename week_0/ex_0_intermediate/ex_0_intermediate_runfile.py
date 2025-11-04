d14a7b7439a5916d83dc7225f1c0e522dbd3f520
ChunLiChung <a.bc96384@gmail.com>
2025-11-04 10:34:31 +0100

    calculation formula finished!

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,8 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+x = int(input("Enter initial_count"))
+y = int(input("Enter final_count"))
+z = int(input("time_min"))
+
+Growth_rate = ( y - x ) / z
+
+print(Growth_rate)
